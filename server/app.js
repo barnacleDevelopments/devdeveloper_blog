dca110339c9b7ea731e6a8ee425ad50c29c913ee
devin <devins.d@protonmail.com>
2021-01-15 06:57:17 -0400

    yup validation for user login and signup

--- server/app.js
+++ server/app.js
@@ -12,8 +12,11 @@ import cors from "cors";
 import path from "path";
 import session from "express-session";
 import passport from 'passport';
-var LocalStrategy = require('passport-local').Strategy;
+
 import cookieParser from "cookie-parser";
+
+// LOCAL STRATEGIES
+import useStrategies from "./config/passport_strategies";
 
 // ROUTES
 import postRoutes from "./routes/post_routes";
@@ -70,53 +73,8 @@ passport.deserializeUser(function (id, done) {
   });
 });
 
-// strategies
-passport.use("local-login", new LocalStrategy({
-  passReqToCallback: true
-},
-  (req, username, password, done) => {
-    User.findOne({ username: username }, (err, user) => {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    });
-  }
-));
-
-passport.use("local-signup", new LocalStrategy({
-  passReqToCallback: true
-},
-  (req, username, password, done) => {
-    console.log(username)
-    User.find({}, (err, users) => {
-      if (err) {
-        return done(err);
-      }
-      if (!err) {
-        let userExists = false;
-        // check if the users exists 
-        users.forEach(user => {
-          user.username === username ? userExists = true : null
-        })
-        // if user does not exist create new user
-        if (!userExists) {
-          User.create({ username: username, password: password }, (err, user) => {
-            console.log(`New user with id: ${user._id}!`)
-            return err ? done(err) : done(null, user)
-          });
-
-        } else {
-          return done(null, false, { message: 'username taken.' });
-        }
-      }
-    })
-  }
-));
+// initialize strategies
+useStrategies();
 
 // User.create({ username: "dev", password: "grape", role: "administrator" }, () => {
 
