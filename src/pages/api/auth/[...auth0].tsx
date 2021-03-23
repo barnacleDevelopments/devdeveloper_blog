70851dcf4e21e2c7529e186052e9c05fe0936884
devin <devins.d@protonmail.com>
2021-03-23 10:39:50 -0300

    ddd

--- src/pages/api/auth/[...auth0].tsx
+++ src/pages/api/auth/[...auth0].tsx
@@ -1,4 +1,4 @@
-import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
+import { handleAuth, handleCallback, handleLogin } from '@auth0/nextjs-auth0';
 // const API_URL = process.env.EXTERNAL_API_URL
 export default handleAuth({
     async login(req, res) {
@@ -9,8 +9,12 @@ export default handleAuth({
                     // Add the `offline_access` scope to also get a Refresh Token
                     scope: 'openid profile email create:category delete:category update:category create:post update:post delete:post create:comment delete:comment', // or AUTH0_SCOPE
                     response_type: "code"
-                }
+                },
+
             });
+            await handleCallback(req, res, {
+                redirectUri: "https://devdeveloper-blog-98c0a9nq2-barnacledevelopments.vercel.app/"
+            })
         } catch (error) {
             res.status(error.status || 400).end(error.message);
         }
